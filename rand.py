9ba299da148c44e67e12c66a0210e8aa6278b434
Tommi Keisala <oss@tkeisala.net>
2013-12-27 12:03:29 +0200

    Add serialport input and draw three acc values

--- rand.py
+++ rand.py
@@ -11,6 +11,9 @@ the left/right mouse buttons. Right click on any plot to show a context menu.
 from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
 import pyqtgraph as pg
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
 #QtGui.QApplication.setGraphicsSystem('raster')
 app = QtGui.QApplication([])
@@ -25,29 +28,46 @@ win.setWindowTitle('pyqtgraph example: Plotting')
 pg.setConfigOptions(antialias=True)
 
 p6 = win.addPlot(title="Updating plot")
-curve = p6.plot(pen='y')
-#data = np.random.normal(size=(1000,1000))
-#data = np.random.normal(size=(10000))
-#print "sizeof (data) " + sizeof(data)
-data = np.arange(1000)
-#data = np.reshape(x, (10,1000))
-print type(data)
-print data.itemsize
+curve_x = p6.plot(pen='r')
+curve_y = p6.plot(pen='g')
+curve_z = p6.plot(pen='b')
+data_x = np.arange(1000)
+data_y = np.arange(1000)
+data_z = np.arange(1000)
+print type(data_x)
+print data_x.itemsize
 ptr = 0
+
 def update():
-    global curve, data, ptr, p6
-    data = np.roll(data, -1)
-    curve.setData(data)
-    if ptr == 0:
-        p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
-    ptr += 1
+    global curve_x, curve_y, curve_z, data_x, data_y, data_z, ptr, p6
+    read = ser.readline()
+    if (read.find('9dof:') == 1):
+	read = read[6:]
+#	print read
+        s_list = read.split(',')
+        print s_list[0] + ':' + s_list[1] + ':' + s_list[2]
+#        s_list = s_list[1].split(',')
+        print int(s_list[0])
+        data_x[999] = int(s_list[0])
+        data_y[999] = int(s_list[1])
+        data_z[999] = int(s_list[2])
+        curve_x.setData(data_x)
+        curve_y.setData(data_y)
+        curve_z.setData(data_z)
+        data_x = np.roll(data_x, -1)
+        data_y = np.roll(data_y, -1)
+        data_z = np.roll(data_z, -1)
+#    if ptr == 0:
+#        p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
+        ptr += 1
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
-timer.start(100)
+timer.start(50)
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
+
 
