bd4db1026e51f4b932811a54c71036fdbadf1101
Tommi Keisala <oss@tkeisala.net>
2013-12-27 16:15:39 +0200

    Add gyroscope and magnerotmeter
    Also cleaning

--- rand.py
+++ rand.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-This example demonstrates many of the 2D plotting capabilities
-in pyqtgraph. All of the plots may be panned/scaled by dragging with 
-the left/right mouse buttons. Right click on any plot to show a context menu.
-"""
-
-#import initExample ## Add path to library (just for examples; you do not need this)
-
 
 from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
@@ -17,46 +9,76 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 
 #QtGui.QApplication.setGraphicsSystem('raster')
 app = QtGui.QApplication([])
-#mw = QtGui.QMainWindow()
-#mw.resize(800,800)
 
-win = pg.GraphicsWindow(title="Basic plotting examples")
-win.resize(1000,600)
-win.setWindowTitle('pyqtgraph example: Plotting')
+win = pg.GraphicsWindow()
+win.resize(1000,300)
+win.setWindowTitle('9dof')
 
 # Enable antialiasing for prettier plots
 pg.setConfigOptions(antialias=True)
 
-p6 = win.addPlot(title="Updating plot")
-curve_x = p6.plot(pen='r')
-curve_y = p6.plot(pen='g')
-curve_z = p6.plot(pen='b')
+p1 = win.addPlot(title="Accelerometer")
+curve_x = p1.plot(pen='r')
+curve_y = p1.plot(pen='g')
+curve_z = p1.plot(pen='b')
 data_x = np.arange(1000)
 data_y = np.arange(1000)
 data_z = np.arange(1000)
-print type(data_x)
-print data_x.itemsize
 ptr = 0
+
+p2 = win.addPlot(title="Gyroscope")
+gyro_curve_x = p2.plot(pen='r')
+gyro_curve_y = p2.plot(pen='b')
+gyro_curve_z = p2.plot(pen='g')
+gyro_data_x = np.arange(1000)
+gyro_data_y = np.arange(1000)
+gyro_data_z = np.arange(1000)
+
+p3 = win.addPlot(title="Magnetometer")
+magn_curve_x = p3.plot(pen='r')
+magn_curve_y = p3.plot(pen='g')
+magn_curve_z = p3.plot(pen='b')
+magn_data_x = np.arange(1000)
+magn_data_y = np.arange(1000)
+magn_data_z = np.arange(1000)
 
 def update():
     global curve_x, curve_y, curve_z, data_x, data_y, data_z, ptr, p6
+    global gyro_curve_x, gyro_curve_y, gyro_curve_z, gyro_data_x
+    global gyro_data_y, gyro_data_z, p2
+    global mang_curve_x, magn_curve_y, magn_curve_z, magn_data_x
+    global magn_data_y, magn_data_z, p3
     read = ser.readline()
     if (read.find('9dof:') == 1):
 	read = read[6:]
-#	print read
         s_list = read.split(',')
-        print s_list[0] + ':' + s_list[1] + ':' + s_list[2]
-#        s_list = s_list[1].split(',')
-        print int(s_list[0])
         data_x[999] = int(s_list[0])
         data_y[999] = int(s_list[1])
         data_z[999] = int(s_list[2])
+	gyro_data_x[999] = int(s_list[3])
+	gyro_data_y[999] = int(s_list[4])
+	gyro_data_z[999] = int(s_list[5])
+	magn_data_x[999] = int(s_list[6])
+	magn_data_y[999] = int(s_list[7])
+	magn_data_z[999] = int(s_list[8])
         curve_x.setData(data_x)
         curve_y.setData(data_y)
         curve_z.setData(data_z)
+	gyro_curve_x.setData(gyro_data_x)
+	gyro_curve_y.setData(gyro_data_y)
+	gyro_curve_z.setData(gyro_data_z)
+	magn_curve_x.setData(magn_data_x)
+	magn_curve_y.setData(magn_data_y)
+	magn_curve_z.setData(magn_data_z)
         data_x = np.roll(data_x, -1)
         data_y = np.roll(data_y, -1)
         data_z = np.roll(data_z, -1)
+        gyro_data_x = np.roll(gyro_data_x, -1)
+        gyro_data_y = np.roll(gyro_data_y, -1)
+        gyro_data_z = np.roll(gyro_data_z, -1)
+        magn_data_x = np.roll(magn_data_x, -1)
+        magn_data_y = np.roll(magn_data_y, -1)
+        magn_data_z = np.roll(magn_data_z, -1)
 #    if ptr == 0:
 #        p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
         ptr += 1
