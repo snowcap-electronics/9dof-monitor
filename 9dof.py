648a963786f6306f1870f5c82ab004d21cff5afd
Tuomas Kulve <tuomas@kulve.fi>
2014-02-13 17:51:14 +0200

    9dof: Update to match new control-board code.
    Use floats and different order for sensor values.

--- 9dof.py
+++ 9dof.py
@@ -24,25 +24,25 @@ p1 = win.addPlot(title="Accelerometer")
 curve_x = p1.plot(pen='r')
 curve_y = p1.plot(pen='g')
 curve_z = p1.plot(pen='b')
-data_x = np.arange(max_values)
-data_y = np.arange(max_values)
-data_z = np.arange(max_values)
+data_x = np.zeros(max_values, dtype='float')
+data_y = np.zeros(max_values, dtype='float')
+data_z = np.zeros(max_values, dtype='float')
 
-p2 = win.addPlot(title="Gyroscope")
-gyro_curve_x = p2.plot(pen='r')
-gyro_curve_y = p2.plot(pen='b')
-gyro_curve_z = p2.plot(pen='g')
-gyro_data_x = np.arange(max_values)
-gyro_data_y = np.arange(max_values)
-gyro_data_z = np.arange(max_values)
+p2 = win.addPlot(title="Magnetometer")
+magn_curve_x = p2.plot(pen='r')
+magn_curve_y = p2.plot(pen='g')
+magn_curve_z = p2.plot(pen='b')
+magn_data_x = np.zeros(max_values, dtype='float')
+magn_data_y = np.zeros(max_values, dtype='float')
+magn_data_z = np.zeros(max_values, dtype='float')
 
-p3 = win.addPlot(title="Magnetometer")
-magn_curve_x = p3.plot(pen='r')
-magn_curve_y = p3.plot(pen='g')
-magn_curve_z = p3.plot(pen='b')
-magn_data_x = np.arange(max_values)
-magn_data_y = np.arange(max_values)
-magn_data_z = np.arange(max_values)
+p3 = win.addPlot(title="Gyroscope")
+gyro_curve_x = p3.plot(pen='r')
+gyro_curve_y = p3.plot(pen='b')
+gyro_curve_z = p3.plot(pen='g')
+gyro_data_x = np.zeros(max_values, dtype='float')
+gyro_data_y = np.zeros(max_values, dtype='float')
+gyro_data_z = np.zeros(max_values, dtype='float')
 
 def update():
     global curve_x, curve_y, curve_z, data_x, data_y, data_z
@@ -54,7 +54,6 @@ def update():
     has_new_raw = 0
     s_list = []
     start_time = time.time()
-
 
     # Read all data from serial
     while (ser.inWaiting()):
@@ -74,37 +73,38 @@ def update():
         data_x = np.roll(data_x, -has_new_raw)
         data_y = np.roll(data_y, -has_new_raw)
         data_z = np.roll(data_z, -has_new_raw)
-        gyro_data_x = np.roll(gyro_data_x, -has_new_raw)
-        gyro_data_y = np.roll(gyro_data_y, -has_new_raw)
-        gyro_data_z = np.roll(gyro_data_z, -has_new_raw)
         magn_data_x = np.roll(magn_data_x, -has_new_raw)
         magn_data_y = np.roll(magn_data_y, -has_new_raw)
         magn_data_z = np.roll(magn_data_z, -has_new_raw)
+        gyro_data_x = np.roll(gyro_data_x, -has_new_raw)
+        gyro_data_y = np.roll(gyro_data_y, -has_new_raw)
+        gyro_data_z = np.roll(gyro_data_z, -has_new_raw)
 
     # Push readings to the data arrays
     for x in range(has_new_raw):
         i = max_values - (has_new_raw - x)
-        data_x[i]      = int(s_list[x][0])
-        data_y[i]      = int(s_list[x][1])
-        data_z[i]      = int(s_list[x][2])
-        gyro_data_x[i] = int(s_list[x][3])
-        gyro_data_y[i] = int(s_list[x][4])
-        gyro_data_z[i] = int(s_list[x][5])
-        magn_data_x[i] = int(s_list[x][6])
-        magn_data_y[i] = int(s_list[x][7])
-        magn_data_z[i] = int(s_list[x][8])
+        data_x[i]      = float(s_list[x][0])
+        data_y[i]      = float(s_list[x][1])
+        data_z[i]      = float(s_list[x][2])
+        magn_data_x[i] = float(s_list[x][3])
+        magn_data_y[i] = float(s_list[x][4])
+        magn_data_z[i] = float(s_list[x][5])
+        gyro_data_x[i] = float(s_list[x][6])
+        gyro_data_y[i] = float(s_list[x][7])
+        gyro_data_z[i] = float(s_list[x][8])
+
 
     # Update plots
     if has_new_raw:
         curve_x.setData(data_x)
         curve_y.setData(data_y)
         curve_z.setData(data_z)
-        gyro_curve_x.setData(gyro_data_x)
-        gyro_curve_y.setData(gyro_data_y)
-        gyro_curve_z.setData(gyro_data_z)
         magn_curve_x.setData(magn_data_x)
         magn_curve_y.setData(magn_data_y)
         magn_curve_z.setData(magn_data_z)
+        gyro_curve_x.setData(gyro_data_x)
+        gyro_curve_y.setData(gyro_data_y)
+        gyro_curve_z.setData(gyro_data_z)
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
